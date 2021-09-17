335dc33f5a966299ed0061316c206357c3f53eb4
-=Oktan=- <r.zhigalenko@mail.ru>
2021-09-18 00:50:25 +0500

    completed ?

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,57 @@
 #include <iostream>
+#include <string>
+
+class Animal
+{
+public:
+    virtual void voice() = 0;
+    virtual ~Animal()
+    {
+        std::cout << "Animal exit" << std::endl;
+    }
+};
+
+class Cat : virtual public Animal
+{
+public:
+    virtual void voice()
+    {
+        std::cout << "Meow ";
+    }
+    virtual ~Cat()
+    {
+        std::cout << "Cat exit" << std::endl;
+    }
+};
+
+class Dog : public Animal
+{
+public:
+    virtual void voice()
+    {
+        std::cout << "Bark ";
+    }
+
+    virtual ~Dog()
+    {
+        std::cout << "Dog exit" << std::endl;
+    }
+};
+
+void meeting(Animal* a, Animal* b)
+{
+    a->voice();
+    b->voice();
+    std::cout << std::endl;
+}
 
 int main()
 {
-    std::cout << "Test" << std::endl;
+    Animal* a = new Dog();
+    Animal* b = new Cat();
+    meeting(a, b);
+
+    delete a;
+    delete b;
     return 0;
 }
